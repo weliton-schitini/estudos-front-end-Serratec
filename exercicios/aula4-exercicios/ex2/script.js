bce3279eba815841f2d0d5a1ea8f3a7816fb593a
weliton schitini <schitini1987@gmail.com>
2024-09-25 18:50:41 -0300

    Adição de setTimeout no segundo exercicio da aula 4

--- exercicios/aula4-exercicios/ex2/script.js
+++ exercicios/aula4-exercicios/ex2/script.js
@@ -7,6 +7,9 @@ let cont = 0
 curtir.addEventListener("click",(()=>{
     document.getElementById("curtidas").innerText = ++cont
    emoticom.src = "../images/star.png"
+   setTimeout(() => {
+    emoticom.src = "../images/happiness.png"
+   }, 1500);
 
 }))
 
@@ -15,7 +18,11 @@ descurtir.addEventListener("click",(()=>{
     if(cont > 0){
          document.getElementById("curtidas").innerText = --cont
     emoticom.src = "../images/sad.png"
+    setTimeout(() => {
+        emoticom.src = "../images/happiness.png"
+       }, 1500);
     }else if(cont == 0){
         emoticom.src = "../images/dead.png"
     }
+    
 }))
