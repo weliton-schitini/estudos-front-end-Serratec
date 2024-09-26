e3509f6dbb1da7aecb5ed13ade98c56b2bf41eca
weliton schitini <schitini1987@gmail.com>
2024-09-26 18:16:23 -0300

    melhoria no codigo desafio eula 5

--- exercicios/aula5-exercicios/exercicio6/script.js
+++ exercicios/aula5-exercicios/exercicio6/script.js
@@ -4,6 +4,24 @@ let lista = document.getElementById("lista")
 
 addBtn.addEventListener("click", () => {
 
+    crateList()
+
+
+       
+})
+
+
+tarefa.addEventListener("keypress", (ele) => {
+    if (ele.key == "Enter") {
+       
+        crateList()
+    }
+
+
+
+})
+
+function crateList(){
     if (tarefa.value != "") {
 
         let li = document.createElement("li")
@@ -22,12 +40,10 @@ addBtn.addEventListener("click", () => {
                 element.target.parentNode.style.textDecoration = "line-through"
                 markBtn.textContent = "Desfazer"
 
-
             } else if (markBtn.textContent == "Desfazer") {
 
                 element.target.parentNode.style.textDecoration = "none"
                 markBtn.textContent = "Completa"
-
 
             }
         })
@@ -39,59 +55,9 @@ addBtn.addEventListener("click", () => {
         dv.append(p, markBtn, removeBtn)
         li.append(dv)
         lista.appendChild(li)
+        tarefa.value = ""
 
     } else {
         alert("Não pode ser adicionado uma tarefa vazia")
     }
-
-
-
-    tarefa.value = ""
-})
-
-
-tarefa.addEventListener("keypress", (ele) => {
-    if (ele.key == "Enter") {
-        if (tarefa.value != "") {
-
-            let li = document.createElement("li")
-
-            let dv = document.createElement("div")
-            let p = document.createElement("p")
-            p.innerText = tarefa.value
-
-
-
-            let markBtn = document.createElement("button")
-            markBtn.textContent = "Completa"
-            markBtn.addEventListener("click", (element) => {
-
-                if (markBtn.textContent == "Completa") {
-                    element.target.parentNode.style.textDecoration = "line-through"
-                    markBtn.textContent = "Desfazer"
-
-                } else if (markBtn.textContent == "Desfazer") {
-
-                    element.target.parentNode.style.textDecoration = "none"
-                    markBtn.textContent = "Completa"
-
-                }
-            })
-
-
-            let removeBtn = document.createElement("button")
-            removeBtn.textContent = "Remover"
-            removeBtn.addEventListener("click", (element) => { element.target.parentNode.remove() })
-            dv.append(p, markBtn, removeBtn)
-            li.append(dv)
-            lista.appendChild(li)
-            tarefa.value = ""
-
-        } else {
-            alert("Não pode ser adicionado uma tarefa vazia")
-        }
-    }
-
-
-
-})
+}
--- exercicios/aula5-exercicios/exercicio6SegundaForma/script.js
+++ exercicios/aula5-exercicios/exercicio6SegundaForma/script.js
@@ -8,72 +8,24 @@ let listaDeTarefas = []
 
 addBtn.addEventListener("click",()=>{
    
-    if(tarefa.value != ""){
-    let tarefaObj = document.createElement("object")
-    tarefaObj.descricao = tarefa.value
-    tarefaObj.completa = false
-    listaDeTarefas.push(tarefaObj)
-
-    let li = document.createElement("li")
-    let div = document.createElement("div")
-    let p = document.createElement("p")
-
-    for(let i = 0; i < listaDeTarefas.length;i++){
-        p.innerText = listaDeTarefas[i].descricao
-        listaDeTarefas[i].completa = true
-       
-    }
-
-    
-
-    let completaBtn = document.createElement("button")
-    completaBtn.innerText  = "Completa"
-
-    let removeBtn = document.createElement("button")
-    removeBtn.innerText = "Remover"
-
-    div.append(p,completaBtn,removeBtn)
-    li.appendChild(div)
-
-    lista.appendChild(li)
-    
-    completaBtn.addEventListener("click",(ele)=>{
-        if(ele.target.innerText == "Completa"){
-            p.style.textDecoration = "line-through"
-            ele.target.innerText = "Desfazer"
-            for(let i = 0;i < listaDeTarefas.length;i++){
-                listaDeTarefas[i].completa = true
-                console.log(listaDeTarefas[i].completa)
-            }
-        }else{
-            p.style.textDecoration = "none"
-            ele.target.innerText = "Completa"
-            for(let i = 0;i < listaDeTarefas.length;i++){
-                listaDeTarefas[i].completa = false
-                console.log(listaDeTarefas[i].completa)
-            }
-        }
-        
-    })
-
-    removeBtn.addEventListener("click",(ele)=>{
-        ele.target.parentNode.remove()
-    
-    })
+    createlist()
 
 
-}else{
-    alert("Não é possível adicionar uma tarefa vazia")
-}
-
-tarefa.value = ""
-
-console.log(listaDeTarefas)
 
 })
 
 tarefa.addEventListener("keypress", (ele) => {
     if(ele.key == "Enter"){
+    createlist()
+
+   
+
+}
+    
+    })
+
+
+function createlist(){
     if(tarefa.value != ""){
         let tarefaObj = document.createElement("object")
         tarefaObj.descricao = tarefa.value
@@ -122,15 +74,4 @@ tarefa.addEventListener("keypress", (ele) => {
         
     }
     tarefa.value = ""
-
-   
-
 }
-    
-    })
-
-
-
-
-
-
